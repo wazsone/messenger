0541471e267a9642957a31a9a9b40274c5cff877
Ruslan Bayandinov <wazsone@ya.ru>
2020-06-29 23:22:40 +0700

    implement error messages with handlebars
     - add css to the public folder in order to use
       with html prepared by handlebars
     - add hello and main templates
     - add error template

--- server.js
+++ server.js
@@ -1,13 +1,35 @@
 const express = require("express");
 const app = express();
 const expressHandlebars = require("express-handlebars");
+const path = require("path");
 
 app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
 app.set("views", "./views");
 app.set("view engine", "handlebars");
 
+app.use(express.static(path.join(__dirname, '/public')));
 app.get("/", (_, res) => {
-    res.render("hello", { greeting: "Hello, my comrad!" });
+    res.render("hello", {
+        title: "Hello!",
+        greeting: "Hello, my comrade!",
+        layout: "hello"
+    });
+});
+
+app.get("/404", (_, res) => {
+    res.render("pages/error", {
+        title: "404",
+        errorCode: "404",
+        errorMessage: "Wrong hit"
+    });
+});
+
+app.get("/500", (_, res) => {
+    res.render("pages/error", {
+        title: "500",
+        errorCode: "500",
+        errorMessage: "Wrong hit"
+    });
 });
 
 const PORT = 4000;
