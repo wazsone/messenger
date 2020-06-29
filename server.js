bae23eac064294b20ab05c843664a3bf6937c104
Ruslan Bayandinov <wazsone@ya.ru>
2020-06-29 23:14:18 +0700

    add handlebars and express-handlebars

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require("express");
-const path = require('path');
-
 const app = express();
+const expressHandlebars = require("express-handlebars");
+
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main" }));
+app.set("views", "./views");
+app.set("view engine", "handlebars");
+
+app.get("/", (_, res) => {
+    res.render("hello", { greeting: "Hello, my comrad!" });
+});
+
 const PORT = 4000;
-
-app.use(express.static('./static'))
-
 app.listen(PORT, function () {
     console.log(`Messenger is listening on port ${PORT}!`);
 });
