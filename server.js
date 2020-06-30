d87ce1c020aed81e7e2050db130336ae06609014
Ruslan Bayandinov <wazsone@ya.ru>
2020-06-30 21:44:24 +0700

    implement user settings page

--- server.js
+++ server.js
@@ -54,6 +54,40 @@ app.get("/sign-up", (_, res) => {
     });
 });
 
+app.get("/user-settings", (_, res) => {
+    res.render("user-settings", {
+        title: "User settings",
+        inputs:[ 
+            {
+                placeholder: "Full name",
+                errorMessage: "Illigal symbols"
+            },
+            {
+                placeholder: "Login",
+                errorMessage: "Illigal symbols"
+            },
+            {
+                placeholder: "E-mail",
+                errorMessage: "Wrong e-mail format"
+            },
+            {
+                placeholder: "Old password",
+                errorMessage: "Wrong password",            },
+            {
+                placeholder: "New password",
+                errorMessage: "Hidden block",
+                className: "hidden"
+            },
+            {
+                placeholder: "New password (again)",
+                errorMessage: "Passwords are not equal"
+            },
+
+        ],
+        confirmBtnName: "Change settings"
+    });
+});
+
 const PORT = 4000;
 app.listen(PORT, function () {
     console.log(`Messenger is listening on port ${PORT}!`);
