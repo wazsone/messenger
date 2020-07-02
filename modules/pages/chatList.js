18c83008c183e4a6695ca83b8cc072da6da8dc7d
Ruslan Bayandinov <wazsone@ya.ru>
2020-07-02 23:08:55 +0700

    implement messages

--- modules/pages/chatList.js
+++ modules/pages/chatList.js
@@ -2,95 +2,109 @@ export function handleChatList(_, res) {
     res.render("chat-list", {
         title: "Chat list",
         logo: "Logo",
+        previews: [
+            {
+                title: "Title",
+                author: "Username",
+                message:
+                    "Sooo long message, let's do it bro, let's write a long long message)",
+            },
+            {
+                title: "Title",
+                author: "Username",
+                message:
+                    "Sooo long message, let's do it bro, let's write a long long message)",
+            },
+            {
+                title: "Title",
+                author: "Username",
+                message:
+                    "Sooo long message, let's do it bro, let's write a long long message)",
+            },
+            {
+                title: "Title",
+                author: "Username",
+                message:
+                    "Sooo long message, let's do it bro, let's write a long long message)",
+            },
+            {
+                title: "Title",
+                author: "Username",
+                message:
+                    "Sooo long message, let's do it bro, let's write a long long message)",
+            },
+            {
+                title: "Title",
+                author: "Username",
+                message:
+                    "Sooo long message, let's do it bro, let's write a long long message)",
+            },
+            {
+                title: "Title",
+                author: "Username",
+                message:
+                    "Sooo long message, let's do it bro, let's write a long long message)",
+            },
+            {
+                title: "Title",
+                author: "Username",
+                message:
+                    "Sooo long message, let's do it bro, let's write a long long message)",
+            },
+            {
+                title: "Title",
+                author: "Username",
+                message:
+                    "Sooo long message, let's do it bro, let's write a long long message)",
+            },
+            {
+                title: "Title",
+                author: "Username",
+                message:
+                    "Sooo long message, let's do it bro, let's write a long long message)",
+            },
+            {
+                title: "Title",
+                author: "Username",
+                message:
+                    "Sooo long message, let's do it bro, let's write a long long message)",
+            },
+            {
+                title: "Title",
+                author: "Username",
+                message:
+                    "Sooo long message, let's do it bro, let's write a long long message)",
+            },
+            {
+                title: "Title",
+                author: "Username",
+                message:
+                    "Sooo long message, let's do it bro, let's write a long long message)",
+            },
+            {
+                title: "Title",
+                author: "Username",
+                message:
+                    "Sooo long message, let's do it bro, let's write a long long message)",
+            },
+            {
+                title: "Title",
+                author: "Username",
+                message:
+                    "Sooo long message, let's do it bro, let's write a long long message)",
+            },
+        ],
         messages: [
             {
-                title: "Title",
-                author: "Username",
-                message:
+                author: "User 1",
+                date: "10:54:55",
+                text:
                     "Sooo long message, let's do it bro, let's write a long long message)",
             },
             {
-                title: "Title",
-                author: "Username",
-                message:
-                    "Sooo long message, let's do it bro, let's write a long long message)",
-            },
-            {
-                title: "Title",
-                author: "Username",
-                message:
-                    "Sooo long message, let's do it bro, let's write a long long message)",
-            },
-            {
-                title: "Title",
-                author: "Username",
-                message:
-                    "Sooo long message, let's do it bro, let's write a long long message)",
-            },
-            {
-                title: "Title",
-                author: "Username",
-                message:
-                    "Sooo long message, let's do it bro, let's write a long long message)",
-            },
-            {
-                title: "Title",
-                author: "Username",
-                message:
-                    "Sooo long message, let's do it bro, let's write a long long message)",
-            },
-            {
-                title: "Title",
-                author: "Username",
-                message:
-                    "Sooo long message, let's do it bro, let's write a long long message)",
-            },
-            {
-                title: "Title",
-                author: "Username",
-                message:
-                    "Sooo long message, let's do it bro, let's write a long long message)",
-            },
-            {
-                title: "Title",
-                author: "Username",
-                message:
-                    "Sooo long message, let's do it bro, let's write a long long message)",
-            },
-            {
-                title: "Title",
-                author: "Username",
-                message:
-                    "Sooo long message, let's do it bro, let's write a long long message)",
-            },
-            {
-                title: "Title",
-                author: "Username",
-                message:
-                    "Sooo long message, let's do it bro, let's write a long long message)",
-            },
-            {
-                title: "Title",
-                author: "Username",
-                message:
-                    "Sooo long message, let's do it bro, let's write a long long message)",
-            },
-            {
-                title: "Title",
-                author: "Username",
-                message:
-                    "Sooo long message, let's do it bro, let's write a long long message)",
-            },
-            {
-                title: "Title",
-                author: "Username",
-                message:
-                    "Sooo long message, let's do it bro, let's write a long long message)",
-            },
-            {
-                title: "Title",
-                author: "Username",
-                message:
+                author: "User 2",
+                date: "10:54:55",
+                text:
                     "Sooo long message, let's do it bro, let's write a long long message)",
             },
         ],
