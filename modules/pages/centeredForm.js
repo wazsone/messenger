f5e8fe104750a4ce072248e79142d6b9397cc14a
Ruslan Bayandinov <wazsone@ya.ru>
2020-07-04 15:41:00 +0700

    improve sign in / sign up forms
     - hide error label by default
     - add names to inputs
     - change input focus style
     - fix confirm button formating

--- modules/pages/centeredForm.js
+++ modules/pages/centeredForm.js
@@ -3,6 +3,10 @@ export function handleSignIn(_, res) {
     res.render("common/centered-form", {
         title: "Sign In",
         placeholders: ["Login", "Password"],
+        inputs: [
+            { name: "login", placeholder: "Login" },
+            { name: "password", placeholder: "Password" },
+        ],
         errorMessage: "Wrong login/password",
         confirmBtnName: "Sign In",
         linkTitle: "Don't have an account?",
@@ -13,6 +17,12 @@ export function handleSignIn(_, res) {
 export function handleSignUp(_, res) {
     res.render("common/centered-form", {
         title: "Sign Up",
+        inputs: [
+            { name: "email", placeholder: "E-mail" },
+            { name: "login", placeholder: "Login" },
+            { name: "password", placeholder: "Password" },
+            { name: "passwordAgain", placeholder: "Password (again)" },
+        ],
         placeholders: ["E-mail", "Login", "Password", "Password (again)"],
         errorMessage: "Passwords are not equal",
         confirmBtnName: "Sign Up",
