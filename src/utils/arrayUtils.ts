efa9122e644de25782b0ea961e351545a54fa0d0
Ruslan Bayandinov <wazsone@ya.ru>
2020-07-18 22:25:30 +0700

    fix array utils

--- src/utils/arrayUtils.ts
+++ src/utils/arrayUtils.ts
@@ -1,63 +1,49 @@
 export class ArrayUtils {
     static last<T>(list: T[]) {
-        if (!list || list.length === 0) {
+        if (ArrayUtils.isEmpty(list)) {
             return undefined;
         }
 
         return list[list.length - 1];
     }
     static first<T>(list: T[]) {
-        if (!list || list.length === 0) {
+        if (ArrayUtils.isEmpty(list)) {
             return undefined;
         }
 
         return list[0];
     }
-    static range(start: number, end: number, step: number, isRight: boolean) {
-        let startValue = 0;
-        let endValue = 0;
-        let count = start;
-        const params = [start, end, step].filter(Number.isFinite);
-        if (params.length > 0) {
-            endValue = Number.isFinite(start) ? start : endValue;
+    static range(start: number, end?: number, step?: number, isReversed?: boolean) {
+        if (end === undefined) {
+            end = start;
+            start = 0;
         }
-        if (params.length > 1) {
-            startValue = Number.isFinite(start) ? start : startValue;
-            endValue = Number.isFinite(end) ? end : endValue;
-            count = endValue;
-        }
-        let stepValue = Math.sign(endValue - startValue);
-        if (params.length > 2) {
-            stepValue = Number.isFinite(step) ? step : stepValue;
-            count =
-                stepValue !== 0
-                    ? Math.floor(endValue / stepValue)
-                    : Math.abs(endValue) - startValue;
-        }
+        step = step !== undefined ? step : (start < end ? 1 : -1);
 
+        let count = Math.max(Math.ceil((end - start) / (step || 1)), 0);
         const result: number[] = [];
-        let value = startValue;
-        let i = 0;
-        while (Math.abs(value) < Math.abs(endValue) && i++ < Math.abs(count)) {
-            result.push(value);
-            value += stepValue;
-        }
+        while (count--) {
+            result.push(start);
+            start += step
+          }
 
-        return isRight ? result.reverse() : result;
+        return isReversed ? result.reverse() : result;
     }
-    static rangeRight(start: number, end: number, step: number) {
+    static rangeRight(start: number, end?: number, step?: number) {
         return ArrayUtils.range(start, end, step, true);
     }
     static isEmpty<T>(value: T) {
         if (value == null || value == undefined) {
             return true;
         }
-        const valueType = typeof value;
-        if (valueType === "object") {
+
+        if (typeof value === "object") {
             return Object.keys(value).length === 0;
-        } else if (valueType === "string") {
-            return !value;
         }
-        return true;
+        if (Array.isArray(value)) {
+            return value.length === 0;
+        }
+
+        return !value;
     }
 }
